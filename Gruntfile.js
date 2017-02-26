b18dbff3000e8e35d0f20dcf7f5fff92626e8cba
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-02-26 13:19:28 +0100

    -grunt ts compiler with options -index.html added -installed http-server -labbing on

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,21 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-ts');
 
     grunt.initConfig({
+        // https://github.com/TypeStrong/grunt-ts/blob/master/sample/Gruntfile.js
         ts: {
             default: {
-                src: ['src/**/*.ts']
+                src: ['src/**/*.ts'],
+                outDir: 'bld/',
+                options: {
+                    // module amd -- this option requires require.js, and is handle async at runtime
+                    // module commonjs -- this option requies a build time module loader like https://www.npmjs.com/package/grunt-browserify
+                    // module system -- this option requires systemjs module loader, https://github.com/systemjs/systemjs
+                    // module '' -- this option does NOT require module loader, BUT IT DOES require the use of namespaces in our .ts files
+                    // infact, the very usage of namespaces hinders module loading for internal code
+                    module: '',
+                    target: 'es5',
+                    fast: 'never',
+                }
             }
         }
     });
--- src/test-a.ts
+++ src/test-a.ts
@@ -1,12 +1,16 @@
-export interface IFoo {
-    bar:string;
-}
+namespace TypesEverywhere {
 
-export class Lorem implements IFoo {
-    bar: string;
-
-    constructor(){
-        this.bar = 'Iam in JavaScript land';
-        console.log(this.bar);
+    export interface IFoo {
+        bar: string;
     }
+
+    export class Lorem implements IFoo {
+        bar: string;
+
+        constructor() {
+            this.bar = 'Iam in JavaScript land!';
+            console.log(this.bar);
+        }
+    }
+
 }
