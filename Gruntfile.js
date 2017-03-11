f5f8980ed263d7d536a836ed0bb4259a476a9913
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-03-11 10:43:44 +0100

    -using tsconfig only -using tsc as npm script

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,33 +7,34 @@ module.exports = function (grunt) {
         // https://github.com/TypeStrong/grunt-ts/blob/master/sample/Gruntfile.js
         ts: {
             default: {
-                src: [
-                    'src/**/*.ts'
-                ],
+                tsconfig: true
+
+                // src: [
+                //     'src/**/*.ts'
+                // ],
                 //outDir: 'bld/',
                 // When bundeling: https://www.npmjs.com/package/grunt-ts#out
                 // Order of files seems to work for us though (test-b.ts is dependant of test-a.ts)
-                out: 'bld/app.js',
-                options: {
-                    // module amd -- this option requires require.js, and is handle async at runtime
-                    // module commonjs -- this option requies a build time module loader like https://www.npmjs.com/package/grunt-browserify
-                    // module system -- this option requires systemjs module loader, https://github.com/systemjs/systemjs
-                    // module '' -- this option does NOT require module loader, BUT IT DOES require the use of namespaces in our .ts files
-                    // infact, the very usage of namespaces hinders module loading for internal code
-                    module: '',
-                    target: 'es5',
-                    fast: 'never',
-                }
+                //out: 'bld/app.js',
+                //options: {
+                                            // module amd -- this option requires require.js, and is handle async at runtime
+                                            // module commonjs -- this option requies a build time module loader like https://www.npmjs.com/package/grunt-browserify
+                                            // module system -- this option requires systemjs module loader, https://github.com/systemjs/systemjs
+                                            // module '' -- this option does NOT require module loader, BUT IT DOES require the use of namespaces in our .ts files
+                                            // infact, the very usage of namespaces hinders module loading for internal code
+                //  module: '',
+                //  target: 'es5',
+                //  fast: 'never',
             }
         },
         uglify: {
             app: {
-                files: { 'bld/app.min.js': ['bld/app.js'] },
-                options: { 
-                    sourceMap: true,
-                    // This gives us source maps back to the individual ts files!
-                    sourceMapIn: 'bld/app.js.map'
-                }
+                files: { 'bld/app.js': ['bld/app.js'] },
+                // options: {
+                //     sourceMap: true,
+                //     // This gives us source maps back to the individual ts files!
+                //     sourceMapIn: 'bld/app.js.map'
+                // }
             }
         }
     });
--- src/test-b.ts
+++ src/test-b.ts
@@ -1,5 +1,3 @@
-/// <reference path="../node_modules/moment/moment.d.ts" />
-
 namespace TypesEverywhere {
 
     export class Lorem {
